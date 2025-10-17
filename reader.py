bbe15d6417d8a387dc845eb0f900f4387c2c1d08
MatTim314 <edston47@gmail.com>
2025-10-17 17:53:51 +0800

    feat: reworked scale reader to use websockets

--- reader.py
+++ reader.py
@@ -1,20 +1,23 @@
 # pip install flask pyserial
-from flask import Flask, jsonify, abort, json, request
+import asyncio
+import random
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import re
 import serial
-import sys
 import subprocess
-import requests
 import datetime
-
+from flask_socketio import SocketIO
 
 BAUDRATE = 9600
 
+app = Flask(__name__)
+CORS(app)
+sio  = SocketIO(app, cors_allowed_origins="*", async_mode="threading")  # or "eventlet"/"gevent"
 
-def log(text):
-    with open("logs.txt", "a") as file:
-        file.write(str(datetime.datetime.now()) + ":" + text)
+def log(msg):
+    with open("logs.txt", "a", encoding="utf-8") as f:
+        f.write(f"{datetime.datetime.now()} : {msg}\n")
    # open once, keep open
 def establish_connection():
     ports = ["COM1", "COM2", "COM3", "COM4", "COM5"]
@@ -25,16 +28,14 @@ def establish_connection():
             ser.port = p
             ser.open()
             if ser.is_open:
-                print(f"Successful connection on port {p}")
+                log(f"Successful connection on port {p}")
                 return ser
-        except serial.serialutil.SerialException as e:
-            print(f"Failed to open on port {p}")
+        except serial.SerialException as e:
+            log(f"Failed to open on port {p}. Error {e}")
             return None
-    return None    
+    return None
 
 
-app = Flask(__name__)
-CORS(app)
 
 
 @app.route("/git")
@@ -46,7 +47,7 @@ def pull_recent_changes():
     cmd = ['git', 'pull']
     result = subprocess.run(cmd,  capture_output=True, text=True, timeout=10)
     log(result.stdout + result.stderr)
-#   return jsonify({'stdout': result.stdout, 'stderr':result.stderr, 'returncode': result.returncode})
+    return jsonify({'stdout': result.stdout, 'stderr':result.stderr, 'returncode': result.returncode})
 
 @app.route("/weight")
 def get_current_weight():
@@ -54,10 +55,11 @@ def get_current_weight():
         buffer = ""
         buffer += ser.read(ser.in_waiting).decode()
         current_weight = 0
-        
+
         if not buffer:
             # Empty buffer comes back as 204
-            return jsonify({}), 204 
+            sio.emit("scale_reading", {"weight": 0})  # broadcast to everyone
+            return jsonify({}), 204
 
         if buffer:
             line = buffer.splitlines()[len(buffer.splitlines()) - 1]
@@ -65,20 +67,61 @@ def get_current_weight():
             if match:
                 current_weight = match.group(1)
 
-        current_weight = int(float(current_weight)* 1000) 
+        current_weight = int(float(current_weight)* 1000)
         return jsonify({"current-weight": current_weight}), 200
     except Exception as e:
         log("Error in getting weight: " + e)
         return jsonify({"error": f"Error printing: {e}"}), 502
 
+@sio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@sio.on('message')
+def handle_message(data):
+    print('Received message:', data)
+    sio.emit('response', 'Server received your message: ' + data)
+
+# ----------------  background job  ----------------
+async def scale_emitter():
+    while True:
+        buffer = ""
+        buffer += ser.read(ser.in_waiting).decode()
+        current_weight = 0
+
+        if not buffer:
+            # Empty buffer comes back as 204
+            sio.emit("scale_reading", {"weight": 0})  # broadcast to everyone
+            return jsonify({}), 204
+
+        if buffer:
+            line = buffer.splitlines()[len(buffer.splitlines()) - 1]
+            match = re.search(r"Weight:\s*([-+]?\d+\.\d+)\s*kg", line, re.IGNORECASE)
+            if match:
+                current_weight = match.group(1)
+
+        current_weight = int(float(current_weight)* 1000)
+        await asyncio.sleep(0.3)
+        sio.emit("scale_reading", {"weight": current_weight})  # broadcast to everyone
+        # val = random.randint(0, 100)
+        # print(val)
+        # sio.emit("scale_reading", {"weight": val})  # broadcast to everyone
+
+def _loop():
+    asyncio.run(scale_emitter())
+
+
 if __name__ == "__main__":
-    pull_recent_changes()
+    # pull_recent_changes()
     ser = establish_connection()
     if ser is None:
         log("Failed to establish connection")
     else:
         log(f"Connection successfull to port {ser.port}")
-    try:
-        app.run(host="0.0.0.0", port=5000)
-    finally:
-        ser.close()
+
+    sio.start_background_task(_loop)
+
+
+    sio.run(app, host="0.0.0.0", port=5000 )
+    # finally:
+    #     ser.close()
